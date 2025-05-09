6810fe588784492762d49c42b9205b91bc1c0e9c
prozecco <prozecco@users.noreply.github.com>
2025-05-09 23:21:43 +0700

    Test

---
+++ locals.py
@@ -0,0 +1,17 @@
+--2025-05-01 06:17:50--  https://git.io/JY9UM
+Loaded CA certificate '/usr/ssl/certs/ca-bundle.crt'
+Resolving git.io (git.io)... 140.82.114.21
+Connecting to git.io (git.io)|140.82.114.21|:443... connected.
+HTTP request sent, awaiting response... 301 Moved Permanently
+Location: https://raw.githubusercontent.com/TeamUltroid/Ultroid/main/resources/startup/locals.py [following]
+--2025-05-01 06:17:51--  https://raw.githubusercontent.com/TeamUltroid/Ultroid/main/resources/startup/locals.py
+Resolving raw.githubusercontent.com (raw.githubusercontent.com)... 2606:50c0:8001::154, 2606:50c0:8002::154, 2606:50c0:8000::154, ...
+Connecting to raw.githubusercontent.com (raw.githubusercontent.com)|2606:50c0:8001::154|:443... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: 3917 (3.8K) [text/plain]
+Saving to: ‘JY9UM.3’
+
+     0K ...                                                   100% 23.0M=0s
+
+2025-05-01 06:17:51 (23.0 MB/s) - ‘JY9UM.3’ saved [3917/3917]
+
